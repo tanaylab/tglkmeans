ff3e9cb348b11478fc9e8213e2b82367c3af851e
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2017-08-13 12:54:18 +0300

    arrange bootstrap code for cmerging with default branch

--- R/tglkmeans-package.r
+++ R/tglkmeans-package.r
@@ -1,4 +1,5 @@
 #' tglkmeans.
+#' 
 #' @import dplyr
 #' @import ggplot2
 #' @importFrom purrr set_names
@@ -6,11 +7,15 @@
 #' @importFrom utils capture.output
 #' @importFrom grDevices colorRampPalette dev.off
 #' @importFrom stats cutree
+#' @importFrom tgstat tgs_cor
+#' 
+#' @useDynLib tglkmeans
+#' @importFrom Rcpp sourceCpp
+#' 
 #' @name tglkmeans
 #' @docType package
 NULL
 
 
-#' @useDynLib tglkmeans
-#' @importFrom Rcpp sourceCpp
-NULL
+
+
