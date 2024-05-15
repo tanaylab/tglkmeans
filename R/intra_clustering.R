44de35194b7dadfca2863a41601188d0f1874af6
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2024-05-15 07:40:32 +0300

    Fix: clustering crashed when `hclust_intra_clusters` was TRUE and input was a matrix.

--- R/intra_clustering.R
+++ R/intra_clustering.R
@@ -1,15 +1,22 @@
 hclust_every_cluster <- function(km, df, parallel = TRUE) {
-    all_hc <- df %>%
-        plyr::dlply(plyr::.(clust), function(x) {
-            ids <- x$id
-            hc <- as.matrix(x[, -1:-2]) %>%
-                t() %>%
-                tgs_cor(pairwise.complete.obs = TRUE, spearman = TRUE) %>%
-                tgs_dist() %>%
-                hclust(method = "ward.D2")
-            return(tibble(clust = x$clust[1], id = ids, intra_clust_order = hc$order))
-        }, .parallel = parallel) %>%
-        purrr::map_df(~.x)
+    doFuture::withDoRNG({
+        all_hc <- df %>%
+            plyr::dlply(plyr::.(clust), function(x) {
+                ids <- x$id
+                dist <- as.matrix(x[, -1:-2]) %>%
+                    t() %>%
+                    tgs_cor(pairwise.complete.obs = TRUE, spearman = TRUE) %>%
+                    tgs_dist()
+
+                if (length(dist) == 0 || any(is.na(dist))) {
+                    return(tibble(clust = x$clust[1], id = ids, intra_clust_order = 1:length(ids)))
+                }
+
+                hc <- hclust(dist, method = "ward.D2")
+                return(tibble(clust = x$clust[1], id = ids, intra_clust_order = hc$order))
+            }, .parallel = parallel) %>%
+            purrr::map_df(~.x)
+    })
 
     res <- df %>%
         select(id, clust) %>%
