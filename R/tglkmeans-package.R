69e424572984d716e2e594461777354d243be6c4
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2026-03-04 15:39:09 +0200

    Replace stats::cor/dist with tgs_cor/tgs_dist, fix pkgdown index
    - Use tgs_cor/tgs_dist for cluster center reordering (was stats::cor + stats::dist)
    - Vectorize predict_tgl_kmeans() using tgs_cor/tgs_dist instead of per-pair cor() loops
    - Remove unused stats::cor and stats::dist imports
    - Add predict_tgl_kmeans, match_clusters, test_clustering to _pkgdown.yml reference index

--- R/tglkmeans-package.R
+++ R/tglkmeans-package.R
@@ -8,7 +8,7 @@
 #' @importFrom purrr set_names
 #' @importFrom Rcpp sourceCpp
 #' @importFrom RcppParallel RcppParallelLibs
-#' @importFrom stats cor dist hclust var
+#' @importFrom stats hclust var
 #' @importFrom tgstat tgs_cor
 #' @importFrom tgstat tgs_dist
 #' @importFrom tibble as_tibble tibble
